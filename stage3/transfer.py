d0531cd7829845a8bd400e2a7ecb804bf45ddce4
molotkova <molotkova548@gmail.com>
2023-03-13 18:14:09 +0100

    uncommented stage 3

--- stage3/transfer.py
+++ stage3/transfer.py
@@ -7,34 +7,34 @@ from keras.preprocessing.image import ImageDataGenerator
 import numpy as np
 import pickle
 
-#if __name__ == '__main__':
-#    # Write your code below
-#    path_model = r'../SavedModels'
-#    path_answer = r'../SavedHistory'
-#    path_data = r'../Data'
-#
-#    # Hyper parameters
-#    batch_size = 64
-#    epochs = 5
-#    img_height = img_width = 150
-#    learning_rate = 1e-3
-#
-#    # Stage3
-#    data_generator = ImageDataGenerator(preprocessing_function=preprocess_input,)
-#
-#    test_data_gen = data_generator.flow_from_directory(
-#        directory=path_data,
-#        classes=['test'],
-#        target_size=(img_height, img_width),
-#        shuffle=False,
-#    )
-#
-#    model = load_model(os.path.join(path_model, 'stage_two_model.h5'))
-#
-#
-#    probabilities = np.argmax(model.predict(test_data_gen), axis=1)
-#
-#    with open("../SavedHistory/stage_three_history", "wb") as file:
-#        pickle.dump(probabilities, file, protocol=pickle.HIGHEST_PROTOCOL)
+if __name__ == '__main__':
+    # Write your code below
+    path_model = r'../SavedModels'
+    path_answer = r'../SavedHistory'
+    path_data = r'../Data'
+
+    # Hyper parameters
+    batch_size = 64
+    epochs = 5
+    img_height = img_width = 150
+    learning_rate = 1e-3
+
+    # Stage3
+    data_generator = ImageDataGenerator(preprocessing_function=preprocess_input,)
+
+    test_data_gen = data_generator.flow_from_directory(
+        directory=path_data,
+        classes=['test'],
+        target_size=(img_height, img_width),
+        shuffle=False,
+    )
+
+    model = load_model(os.path.join(path_model, 'stage_two_model.h5'))
+
+
+    probabilities = np.argmax(model.predict(test_data_gen), axis=1)
+
+    with open("../SavedHistory/stage_three_history", "wb") as file:
+        pickle.dump(probabilities, file, protocol=pickle.HIGHEST_PROTOCOL)
 
 
